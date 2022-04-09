678476e945a7b0f3b2e37235bc23d65641a6d507
vaibav <vaibav@192.168.1.42>
2022-04-09 13:33:04 -0500

    ye4s

---
+++ HelloWorld/src/Hellow.java
@@ -0,0 +1,87 @@
+
+public class Hellow {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hello World");
+		
+		
+		
+	int cars;
+	
+	cars = 5;
+	
+	
+	
+		
+	//primitive data types
+	int hello_world = 9;
+	double num2 = 5.0;
+	boolean b = true;
+	char c = 't';  // any letter or character, ONLY ONE CHARACTER
+	//not primitive
+	String str = "9";
+	
+	
+	int tim = hello_world;
+	
+	System.out.println(hello_world);
+	
+		
+		
+	
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+if (cars==5) {
+		
+		System.out.println("This car is right");
+	}
+	
+	else {
+		System.out.println("This car is wrong");
+	}
+	
+	
+	}
+	
+	
+}
---
+++ HelloWorld/src/basicOpoerators.java
@@ -0,0 +1,38 @@
+
+public class basicOpoerators {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		
+		double x = 5;
+		double y = 7;
+		double z = 56;
+		double sum = x / y * z;
+		double here = x / y;
+		
+	//	System.out.println(sum);
+	//	System.out.println(here);
+		
+		
+		double d = Math.pow(x, y);
+		
+		
+		//System.out.print(d);
+		
+		
+		
+		int a = 5;
+		int b = 7;
+		int c = 56;
+		int u = a % b;
+		
+		System.out.println(u);
+		
+		
+		
+		
+	}
+
+}
